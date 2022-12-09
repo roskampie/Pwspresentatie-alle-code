5e15e3fe26bca78402b72db15864e4b5aec4f924
wouter <wouter@roskam.nu>
2022-12-09 12:27:21 +0100

    onzin verwijderd

--- CMD/test4.py
+++
@@ -1,9 +0,0 @@
-# Import the os module
-import os
-
-cwd = os.getcwd()
-
-print("Current working directory: {0}".format(cwd))
-
-print("os.getcwd() returns an object of type: {0}".format(type(cwd)))
-#dit is wel goeie code ja, wle handig om te weten voor latere dingen. lekker bezig
